895788803a2de28ede91af789b65bf6fdbafeb98
Rohan belsare <rohanbelsare113@gmail.com>
2026-09-24 01:44:43 +0530

    Fix MainActivity compilation error

--- android/app/src/main/java/com/rohan/nascontroller/MainActivity.java
+++ android/app/src/main/java/com/rohan/nascontroller/MainActivity.java
@@ -1,21 +1,5 @@
 package com.rohan.nascontroller;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-// 1. Import the NodeJS plugin
-import com.capawesome.capacitor.nodejs.NodeJS;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // 2. Start the Node.js server (pointing to your server.js file)
-        NodeJS.getInstance().start(this, "server.js", new com.capawesome.capacitor.nodejs.NodeJSStartupListener() {
-            @Override
-            public void onStartup() {
-                // Node.js is running successfully
-            }
-        });
-    }
-}
+public class MainActivity extends BridgeActivity {}
